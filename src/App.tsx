939a2a7c95d8f999986c7461b2c2212f352b0018
tthugy <supermanace7@gmail.com>
2022-04-24 23:13:14 +0900

    2021-04-24 generic

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 
 function App() {
+  type SuperPrint = {
+    <T>(arr: T[]): void
+  }
+
+  const superPrint: SuperPrint = (arr) => {
+    arr.forEach(i => console.log(i))
+  }
+
+  superPrint([1, 2, 3, 4])
+  superPrint([true, false, true])
+  superPrint(["a", "b", "c"])
+  superPrint([1, 2, true, false, "hello"])
+
   return (
     <div>
       blockchain
