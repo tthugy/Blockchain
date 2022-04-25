a85b0e6101bb09174ba97308caa0bee76e0a2be3
tthugy <supermanace7@gmail.com>
2022-04-25 23:39:57 +0900

    2022-04-25 classes 2

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,42 @@ import React from 'react';
 
 function App() {
 
-  abstract class User {
-    constructor(
-      protected firstName: string,
-      protected lastName: string,
-      protected nickname: string
-    ){}
-    abstract getNickName():void
-    getFullName(){
-      return `${this.firstName} ${this.lastName}`
-    }
+  type Words = {
+    [key: string]: string
   }
-  class Player extends User {
-    getNickName(): void {
-      console.log(this.nickname)
-    }
-  }
-  const won = new Player("Wonseok", "Choi", "tthugy");
 
-  const name = won.getFullName();
+  class Dict {
+    private words: Words
+    constructor(){
+      this.words = {}
+    }
+    add(word: Word){
+      if(this.words[word.term] === undefined){
+        this.words[word.term] = word.def;
+      }
+    }
+    def(term: string){
+      return this.words[term]
+    }
+  }
+
+  class Word {
+    constructor(
+      public term: string,
+      public def: string
+    ) {}
+  }
+
+  const kimchi = new Word("kimchi", "한국의 음식");
+
+  const dict = new Dict()
+
+  dict.add(kimchi);
+  dict.def("kimchi");
 
   return (
     <div>
-      {name}
+
     </div>
   );
 }
